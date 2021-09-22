42bc17fdca09a95f556bacebf4979aae98446222
Shivamjha <34316298+iamshiv4m@users.noreply.github.com>
2021-09-23 00:01:48 +0530

    'initial'

---
+++ routes/files.js
@@ -0,0 +1,88 @@
+const router = require("express").Router();
+const multer = require("multer");
+const path = require("path");
+const File = require("../models/file");
+const { v4: uuid4 } = require("uuid");
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
+  },
+});
+
+let upload = multer({
+  storage,
+  limit: { fileSize: 1000000 * 100 },
+}).single("myfile");
+
+router.post("/", (req, res) => {
+  upload(req, res, async (err) => {
+    //   Validate Request
+    if (!req.file) {
+      return res.json({ error: "All Fields are Mandotary" });
+    }
+    // store files
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+
+    // store into database
+
+    const file = new File({
+      filename: req.file.filename,
+      uuid: uuid4(),
+      path: req.file.path,
+      size: req.file.size,
+    });
+
+    const response = await file.save();
+    return res.json({
+      file: `${process.env.APP_BASE_URL}/files/${response.uuid}`,
+    });
+  });
+
+  // store into database
+
+  // response -> Link
+});
+
+router.post("/send", async (req, res) => {
+  const { uuid, emailTo, emailFrom } = req.body;
+  // validate request
+  if (!uuid || !emailTo || !emailFrom) {
+    return res.status(422).send({ error: "All fields are required" });
+  }
+
+  // Get Data from DataBase
+  const file = await File.findOne({ uuid: uuid });
+  if (file.sender) {
+    return res.status(422).send({ error: "Email already sent" });
+  }
+
+  file.sender = emailFrom;
+  file.receiver = emailTo;
+  const response = await file.save();
+
+  // send email
+
+  const sendMail = require("../services/emailService");
+  sendMail({
+    from: emailFrom,
+    to: emailTo,
+    subject: "FileSharing via link",
+    text: `${emailFrom} shared a file with you`,
+    html: require("../services/emailTemplate")({
+      emailFrom: emailFrom,
+      downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
+      size: parseInt(file.size / 1000) + "kb",
+      expires: "24 hours",
+    }),
+  });
+  return res.send({ success: true });
+});
+
+module.exports = router;
---
+++ services/emailService.js
@@ -0,0 +1,24 @@
+const nodeMailer = require("nodemailer");
+
+async function sendMail({ from, to, subject, text, html }) {
+  let transporter = nodeMailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    //   key or value same hai toh ek baar likh sakte hai
+    from: `FileShare<${from}>`,
+    to,
+    subject,
+    text,
+    html,
+  });
+}
+
+module.exports = sendMail;
